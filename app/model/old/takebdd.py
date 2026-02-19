39dddbf2ad3fb95da540491d7d9add14001607ae
NIX3S <77367129+NIX3S@users.noreply.github.com>
2026-02-19 23:51:33 +0100

    Delete app/model/old directory

--- app/model/old/takebdd.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-from data import InputData
-# Boucle pour générer SQLAlchemy columns
-for field_name, field_info in InputData.model_fields.items():
-    print(f"{field_name} = Column(Float)")
